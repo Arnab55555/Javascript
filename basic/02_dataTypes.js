a6fefd2b585a68ae23f898b56cea42d64f0a06ca
Arnab55555 <arnabsamanta555@gmail.com>
2025-08-29 17:36:09 +0530

    datatypes

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+
+let name = "John"; //string
+let age = 25; //number
+let isLoggedIn = false; //boolean
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53, mostly used in stocks, bank transactions
+// string => "", '', `` mostly use "" for single line and `` for multi line
+// boolean => true/false
+// null => standalone value, not object, means empty value
+// undefined => variable not assigned any value
+// symbol => unique identifier
+
+
+// object => key value pairs
+
+console.log(typeof undefined); // output: undefined
+
+console.log(typeof null); // output: object (bug in js, should be null)
+
+
