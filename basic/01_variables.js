3d5d0442d93b531a016226928cded05e0b2078d6
Arnab55555 <arnabsamanta555@gmail.com>
2025-08-29 16:50:47 +0530

    Variables info file

---
+++ basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "abc@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+let accountState; // Variable declared but not defined
+
+// accountId = 2  // Variable cannot be changed if declared using const
+
+accountEmail = "xyz@gmail.com"
+
+accountPassword = "67890"
+
+accountCity = "Navi Mumbai"
+
+console.log(accountId);
+
+/*
+Prefer not to use var for variable declaration
+because of issue with block scope 
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
